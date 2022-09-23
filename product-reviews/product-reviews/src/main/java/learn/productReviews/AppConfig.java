211018d179b85d85f0547d49632eeb30a698e169
Monica Boose <monicasboose@gmail.com>
2022-09-23 15:23:25 -0500

    Created AppUser, Review, Product models

--- product-reviews/product-reviews/src/main/java/learn/productReviews/AppConfig.java
+++ product-reviews/product-reviews/src/main/java/learn/productReviews/AppConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
+
     @Bean
     public PasswordEncoder getEncoder(){
         return new BCryptPasswordEncoder();
--- product-reviews/product-reviews/src/main/java/learn/productReviews/models/Review.java
+++ product-reviews/product-reviews/src/main/java/learn/productReviews/models/Review.java
@@ -1,4 +1,53 @@
 package learn.productReviews.models;
 
+import java.time.LocalDate;
+
 public class Review {
+
+    private int id;
+    private AppUser user;
+    private LocalDate date;
+    private String content;
+
+    public Review() {
+    }
+
+    public Review(int id, AppUser user, LocalDate date, String content) {
+        this.id = id;
+        this.user = user;
+        this.date = date;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public AppUser getUser() {
+        return user;
+    }
+
+    public void setUser(AppUser user) {
+        this.user = user;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
