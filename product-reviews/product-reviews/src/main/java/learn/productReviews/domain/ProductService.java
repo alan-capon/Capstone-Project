23a8811916452dac4b7e78bc2cda44d47f993f64
Monica Boose <monicasboose@gmail.com>
2022-09-26 11:36:09 -0500

    Completed ProductService

--- product-reviews/product-reviews/src/main/java/learn/productReviews/domain/ProductService.java
+++ product-reviews/product-reviews/src/main/java/learn/productReviews/domain/ProductService.java
@@ -31,13 +31,40 @@ public class ProductService {
 
         Result<Product> result = new Result<>();
 
+        if (product != null && product.getId() > 0){
+            result.addErrorMessage("Product ID should not be set.", ResultType.INVALID);
+        }
+
+        if (result.isSuccess()){
+            result.setPayload(productRepository.add(product));
+        }
+
+        return result;
+    }
+
+    public Result<Product> update(Product product){
+
+        Result<Product> result = validate(product);
+
+        if (product.getId() <= 0){
+            result.addErrorMessage("Product ID is required.", ResultType.INVALID);
+        }
+
+        if (result.isSuccess()){
+            if (productRepository.update(product)){
+                result.setPayload(product);
+            } else {
+                result.addErrorMessage("Product %s was not found.", ResultType.NOT_FOUND, product.getId());
+            }
+        }
+        return result;
     }
 
     public Result<Product> deleteById(int id){
 
         Result<Product> result = new Result<>();
         if (!productRepository.deleteById(id)){
-            result.addErrorMessage("Product %s was not found", ResultType.NOT_FOUND, id);
+            result.addErrorMessage("Product %s was not found.", ResultType.NOT_FOUND, id);
         }
         return result;
     }
@@ -46,12 +73,12 @@ public class ProductService {
 
         Result<Product> result = new Result<>();
         if(product == null){
-            result.addErrorMessage("Product cannot be null", ResultType.INVALID);
+            result.addErrorMessage("Product cannot be null.", ResultType.INVALID);
             return result;
         }
 
         if(product.getName() == null || product.getName().isBlank()){
-            result.addErrorMessage("Product name is required", ResultType.INVALID);
+            result.addErrorMessage("Product name is required.", ResultType.INVALID);
         }
 
         return result;
